74c84a7ea28c7b464628d680a81d1a27e7353070
Levy <85988919231levy@gmail.com>
2025-09-27 00:11:08 -0300

    configurando prisma e estrurando tabela

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -1,1 +1,9 @@
-//
+import { PrismaClient } from "@prisma/client";
+
+export class Prisma {
+    private prisma = new PrismaClient()
+
+    async connection () {
+        return this.prisma
+   }
+}
--- api/src/repository/Contact.Repository.ts
+++ api/src/repository/Contact.Repository.ts
@@ -1,1 +1,8 @@
-//
+import { Prisma } from "../config/database.js";
+export class ContactRepository {
+    constructor (private readonly prisma =  new Prisma) {}
+
+    async find() {
+        
+    }
+}
